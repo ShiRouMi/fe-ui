88a261ae8ea593c57435817fc59cf6f5b36456c1
fengye <719130272@qq.com>
2019-10-22 11:48:14 +0800

    feat：单独使用的 checkbox

--- src/components/Checkbox/checkbox.vue
+++ src/components/Checkbox/checkbox.vue
@@ -1,115 +1,131 @@
 <template>
-  <label class="fe-checkbox">
-    <span class="fe-checkbox__input" :class="{'is-checked': isChecked}">
-      <!-- 多选框的实现 -->
-      <span class="fe-checkbox__inner" ></span>
-      <input type="checkbox" class="fe-checkbox__original" :checked="isChecked" @click="clickCheckbox">
+  <label>
+    <span 
+      class="checkbox__input"
+      :class="{
+        'is-checked': isChecked,
+        'is-disabled': isDisabled,
+        'is-focus': focus
+      }">
+      <span class="checkbox__inner"></span>
+      <input
+        class="checkbox__original"
+        type="checkbox"
+        :disabled="disabled"
+        v-model="model"
+        @change="change"
+        @focus="focus = true"
+        @blur="focus = false" />
+
     </span>
-    <slot>
-      <span class="fe-checkbox__label">
-      </span>
-    </slot>
+    <span class="checkbox__label">
+      <slot></slot>
+    </span>
   </label>
 </template>
 
 <script>
 export default {
-  name: 'checkbox',
+  name: 'fCheckbox',
   props: {
-    value: Boolean,
-    label: [String, Number, Boolean],
-    trueLabel: [String, Number],
-    falseLabel: [String, Number],
-    disabeld: Boolean,
-    border: Boolean,
-    size: {
-      type: String,
-      default: 'medium' // [medium, small, mini]
+    disabled: {
+      type: Boolean,
+      default: false
     },
-    name: String,
-    checked: Boolean,
-    indeterminate: Boolean
+    value: {
+      type: [String, Number, Boolean],
+      default: false
+    }
+  },
+  data: function() {
+    return {
+      selfModel: false,
+      focus: false
+    }
   },
   computed: {
+    model: {
+      get() {
+        return this.value !== undefined 
+          ? this.value 
+          : this.selfModel
+      },
+      set(val) {
+        this.$emit('input', val)
+        this.selfModel = val
+      }
+    },
     isChecked() {
-      return this.value || this.checked
+      return this.model
+    },
+    isDisabled() {
+      return this.disabled
     }
   },
   methods: {
-    clickCheckbox() {
-      this.$emit('change', this.value)
+    change(event) {
+      if(this.disabled) return false
+
+      let checked = event.target.checked
+  
+      this.$emit('on-change', checked)
     }
   }
 }
 </script>
-
-<style lang="scss">
-.fe-checkbox {
-  color: #606266;
-  font-weight: 500;
-  font-size: 14px;
-  position: relative;
+<style scoped lang="scss">
+.checkbox__input {
   cursor: pointer;
+  vertical-align: middle;
+  line-height: 1
+}
+.checkbox__input.is-focus .checkbox__inner {
+  border-color: #409eff;
+}
+.checkbox__input.is-checked .checkbox__inner {
+  background-color: #409eff;
+  border-color: #409eff;
+
+  &:after {
+    transform: rotate(45deg) scaleY(1);
+  }
+}
+.checkbox__inner {
+  position: relative;
   display: inline-block;
-  white-space: nowrap;
-  user-select: none;
+  border: 1px solid #DCDFE6;
+  border-radius: 2px;
+  width: 14px;
+  height: 14px;
+  background: #fff;
 
-  &__input {
-    white-space: nowrap;
-    cursor: pointer;
-    outline: none;
-    display: inline-block;
-    line-height: 1;
-    position: relative;
-    vertical-align: middle;
-
-    &.is-checked .fe-checkbox__inner {
-      background-color: #409eff;
-      border-color: #409eff;
-    }
-
-    &.is-checked .fe-checkbox__inner:after {
-      transform: rotate(45deg) scaleY(1);
-    }
-  }
-
-  &__inner {
-    display: inline-block;
-    position: relative;
-    border: 1px solid #dcdfe6;
-    border-radius: 2px;
-    box-sizing: border-box;
-    width: 14px;
-    height: 14px;
-    background-color: #fff;
-    z-index: 1;
-    transition: border-color .25s cubic-bezier(.71,-.46,.29,1.46),background-color .25s cubic-bezier(.71,-.46,.29,1.46);
-
-    &:after {
-      box-sizing: content-box;
-      content: "";
-      border: 1px solid #fff;
-      border-left: 0;
-      border-top: 0;
-      height: 7px;
-      left: 4px;
-      position: absolute;
-      top: 1px;
-      transform: rotate(45deg) scaleY(0);
-      width: 3px;
-      transition: transform .15s ease-in .05s;
-      transform-origin: center;
-    }
-  }
-
-  &__original {
-    opacity: 0;
-    outline: none;
+  &:after {
+    box-sizing: content-box;
+    content: "";
+    border: 1px solid #fff;
+    border-left: 0;
+    border-top: 0;
+    height: 7px;
+    left: 4px;
     position: absolute;
-    margin: 0;
-    width: 0;
-    height: 0;
-    z-index: -1;
+    top: 1px;
+    transform: rotate(45deg) scaleY(0);
+    width: 3px;
+    transition: transform .15s ease-in .05s;
+    transform-origin: center;
   }
+}
+.checkbox__original {
+  opacity: 0;
+  position: absolute;
+  margin: 0;
+  width: 0;
+  height: 0;
+}
+.checkbox__label {
+  display: inline-block;
+  padding-left: 10px;
+  line-height: 19px;
+  font-size: 14px;
 }
 </style>
